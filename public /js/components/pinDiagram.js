d5e064afa82adf9ff062129eceb7c49947d1e824
Atomsk Ed Shannon <flapjack_finance@proton.me>
2025-07-29 19:16:52 -0700

    Update pinDiagram.js

--- public /js/components/pinDiagram.js
+++ public /js/components/pinDiagram.js
@@ -1,32 +1,27 @@
 import { esp32Pins } from '../config/pinDefinitions.js';
 import { showPinConfiguration } from './pinConfigPanel.js';
 
-
-let selectedPin = null;
-
-function createPinDiagram() {
+function initializePinDiagram() {
     const diagram = document.getElementById('pinDiagram');
     diagram.innerHTML = '';
 
     Object.entries(esp32Pins).forEach(([pinNumber, pinData]) => {
         const pinButton = document.createElement('div');
         pinButton.className = 'pin-button';
-        pinButton.onclick = () => selectPin(pinNumber);
+        pinButton.onclick = () => {
+            document.querySelectorAll('.pin-button').forEach(btn => btn.classList.remove('selected'));
+            pinButton.classList.add('selected');
+            showPinConfiguration(pinNumber);
+        };
 
         pinButton.innerHTML = `
             <div class="pin-name">${pinData.name}</div>
             <div class="pin-type">${pinData.type}</div>
         `;
-
         diagram.appendChild(pinButton);
     });
 }
 
-function selectPin(pinNumber) {
-    document.querySelectorAll('.pin-button').forEach(btn => btn.classList.remove('selected'));
-    event.target.closest('.pin-button').classList.add('selected');
-    selectedPin = pinNumber;
-    showPinConfiguration(pinNumber);
-}
+export { initializePinDiagram };
 
 export { createPinDiagram, selectedPin };
